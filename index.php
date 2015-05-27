4c55f87baa0a7e313998bf8b115209e5d554d0b8
Chimo <chimo@chromic.org>
2015-05-26 23:03:41 -0400

    Crude "filter" implementation

--- index.php
+++ index.php
@@ -6,8 +6,6 @@
 </head>
 <body>
 
-
-
 <?php
 
 require_once('config.php');
@@ -22,7 +20,38 @@ try {
     die('Connection failed: ' . $e->getMessage());
 }
 
-$sql = 'SELECT title, content, published, foreign_url, subscription.type FROM event INNER JOIN subscription ON event.subscription_id = subscription.id ORDER BY published DESC';
+$sql = 'SELECT DISTINCT type FROM subscription';
+$res = $dbh->query($sql);
+$types = array();
+
+$html = '<details><summary>Filters</summary><form>';
+
+while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+    $types[] = $row['type'];
+}
+
+$filters = $_GET['filter'];
+
+$where = '';
+
+foreach($types as $type) {
+    $html .= '<label><input type="checkbox" name="filter[]" value="' . $type . '" ';
+
+    if (!$filters || in_array($type, $filters)) {
+        $html .= "checked";
+
+        $where .= 'OR subscription.type = "' . $type . '"';
+    }
+
+    $html .= ' "> ' . $type . '</label>';
+}
+
+$where = ' WHERE ' . substr($where, 2);
+
+$html .= '<input type="submit"></form></details>';
+echo $html;
+
+$sql = 'SELECT title, content, published, foreign_url, subscription.type FROM event INNER JOIN subscription ON event.subscription_id = subscription.id' . $where . ' ORDER BY published DESC';
 
 $res = $dbh->query($sql);
 $html = '<ol id="list">';
