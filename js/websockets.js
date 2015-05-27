a6cf89cf6078457ea8f9a2ec8eda8d8a2fff89d7
Chimo <chimo@chromic.org>
2015-05-26 22:14:42 -0400

    Formatting

--- js/websockets.js
+++ js/websockets.js
@@ -18,7 +18,7 @@
         };
 
         ws.onopen = function( /*event*/ ) {
-          console.log( "connection established" );
+            console.log( "connection established" );
         };
 
         ws.onerror = function( /*event*/ ) {
@@ -32,8 +32,8 @@
             console.log( "WebSocket connection closed" + closedDetails );
             console.log( "Code: " + event.code + " ( see: https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent )" );
 
-            // Don't print reason if it's an empty string
-            if ( reason === "" ) {
+            // Don't print reason if we don't have one
+            if ( reason ) {
                 console.log( "Reason: " + event.reason );
             }
         };
